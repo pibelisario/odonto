1069e693c8176f5ea62be81b04c0329bcef25cbf
Paulo Inácio <108241856+pibelisario@users.noreply.github.com>
2023-05-17 22:16:46 -0300

    Arrumando css tela editarPaciente

--- src/main/java/br/caixa/odonto/controllers/indexController.java
+++ src/main/java/br/caixa/odonto/controllers/indexController.java
@@ -2,13 +2,11 @@ package br.caixa.odonto.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("index")
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("index")
     public String index() {
         return "index.html";
     }
